5f45aa7102594a3bf8a200a76df532cba4918515
fatima2419 <126586807+fatima2419@users.noreply.github.com>
2023-08-11 23:25:13 +0300

    new update

--- myhw/src/App.js
+++ myhw/src/App.js
@@ -1,13 +1,49 @@
-import {
-  useState,
-  useRef
-} from "react"; 
+
+import React, { useState } from 'react';
 import "./App.css";
 
 function App() { 
-  
+  const cartoonname = [
+    'Naruto',
+    'demon slayer',
+    'fire force',
+    'last air bender',
+    'black clover'
+  ];
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredItems, setFilteredItems] = useState(cartoonname);
+
+  const handleSearch = (e) => {
+    const searchTerm = e.target.value.toLowerCase();
+    setSearchTerm(searchTerm);
+
+    const filteredItems = cartoonname.filter(item =>
+      item.toLowerCase().includes(searchTerm));
+      // لتنسين فكرة الفلتر راح ياخذ الاري الاصلي ماتج ياخذ ايتم ايتم يحولة 
+      //سمول لتر ويشوف منو يحتوي عل الحرف الي دحلتي بالسيرج
+       //او كلمة لان السيرج ايتم الفاليو ماته مرتبطة بالانبت سيرج
+    setFilteredItems(filteredItems);
+  };
   return ( 
-    <><h1>hello world</h1></>
+    <div className="cartoon">
+<h3>my cartoonlist</h3>
+      <input
+      className='search'
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={handleSearch}
+      />
+      <ul>
+        {/* هنا سوينا ماب لان راح يدحل على الاري ماتي الجديد على كل ايلمن 
+         بي ويطبق اوبريشن عليه ال ايتم ,هي ارو فنكسن راح تطبق على كل ايتم ب الاري الجديد  
+         */}
+    {filteredItems.map((item) => (
+          <li>{item}</li>
+        ))}
+</ul>
+    </div>
+    
   );
 } 
  
