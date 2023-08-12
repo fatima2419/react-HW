2a5c5a38ef97b9f55bc0999cb1e41dee1097cf9e
fatima2419 <126586807+fatima2419@users.noreply.github.com>
2023-08-12 12:32:24 +0300

    online lec sign in form

---
+++ myhw/src/Sign.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import React from "react";
+function Sign(){
+    const [name,setName]=useState('fatima')
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+
+    const handlechangename =(e)=>{
+        setName(e.target.value);
+    } 
+    const handlechangeemail =(e)=>{
+        setEmail(e.target.value);
+    }
+    const handlechangepassword =(e)=>{
+        setPassword(e.target.value);
+    }
+    const handlechangensubmit =(e)=>{
+        console.log('you are in')
+    }
+return(
+    <>
+    <form onSubmit={handlechangensubmit}>
+        <div>
+            <label>Name</label>
+            <input type="text" name="name" value={name}
+        onChange={handlechangename}/>
+        </div> 
+        <div>
+            <label>email</label>
+            <input type="email" name="email" value={email}
+        onChange={handlechangeemail}/>
+        </div>
+        <div>
+            <label>password</label>
+            <input type="password" name="password" value={password}
+        onChange={handlechangepassword}/>
+        </div>
+<button type="submit" >Submit</button>
+        </form></>
+)
+}
+
+export default Sign;
